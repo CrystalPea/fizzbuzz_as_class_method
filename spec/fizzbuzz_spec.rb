c638269604b8c62d1e44e7cf9eec82f446b7aa1b
Pea <m.a.tyczynska@gmail.com>
2016-11-04 16:17:06 +0000

    Test 3 created: RED

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -8,4 +8,8 @@ describe 'fizzbuzz' do
   it 'returns integer when passed number that is not divisible by 3 or 5 as an argument' do
     expect(4.fizzbuzz).to eq 4
   end
+  
+  it 'returns "buzz" when passed multiplication of 5 as an argument' do
+    expect(5.fizzbuzz).to eq "buzz"
+  end
 end
