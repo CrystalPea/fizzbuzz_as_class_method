f1e2b91feaec3f0624f7f18133aeb284e987ddb5
James <jamesdix@cwgsy.net>
2016-11-04 16:11:07 +0000

    Refactor test code - new test added

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,9 +4,8 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3 as an argument' do
     expect(3.fizzbuzz).to eq "fizz"
   end
-end
 
-describe 'fizzbuzz' do
-  it 'returns integer when passed number that is not divisible by 3 or 5 as an argument'
+  it 'returns integer when passed number that is not divisible by 3 or 5 as an argument' do
     expect(4.fizzbuzz).to eq 4
+  end
 end
